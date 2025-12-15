c195d85cbc7e44a1b39381544de4bde642cdb840
kdeleo <kseniadeleo@gmail.com>
2025-12-15 15:39:55 +0100

    add CRAB config files

---
+++ HeavyIonsAnalysis/Configuration/test/crabJobs/crab_data_ZMM_PPRefSingleMuon2.py
@@ -0,0 +1,26 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'test16_ZMM_PPRefSingleMuon2'
+
+config.JobType.pluginName = 'Analysis'
+# Name of the CMSSW configuration file
+config.JobType.psetName = 'forest_miniAOD_run3_ppref_DATA_ZMM.py'
+config.JobType.maxMemoryMB = 2500
+config.JobType.maxJobRuntimeMin = 1200
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/PPRefSingleMuon2/Run2024J-PromptReco-v1/MINIAOD'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 10
+config.Data.publication = False
+config.Data.inputDBS = 'global'
+# This string is used to construct the output dataset name
+config.Data.outputDatasetTag = 'CRAB3_Analysis_test16_ZMM_PPRefSingleMuon2'
+
+# These values only make sense for processing data
+#    Select input data based on a lumi mask
+config.Data.lumiMask = 'Cert_Collisions2024_ppref_387474_387721_golden.json'
+
+# Where the output files will be transmitted to
+config.Site.storageSite = 'T3_IT_Trieste'
---
+++ HeavyIonsAnalysis/Configuration/test/crabJobs/crab_mc_ZMM_SingleT.py
@@ -0,0 +1,26 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'test18_mc_ppRef_ZMM_SingleT'
+
+config.JobType.pluginName = 'Analysis'
+# Name of the CMSSW configuration file
+config.JobType.psetName = 'forest_miniAOD_run3_ppref_MC_ZMM.py'
+config.JobType.maxMemoryMB = 2500
+config.JobType.maxJobRuntimeMin = 1200
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset ='/T_TuneCP5_5p36TeV_powheg-pythia8/RunIIIpp5p36Winter24MiniAOD-141X_mcRun3_2024_realistic_ppRef5TeV_v7-v1/MINIAODSIM'
+config.Data.splitting = 'FileBased'
+config.Data.unitsPerJob = 1
+config.Data.publication = False
+config.Data.inputDBS = 'global'
+# This string is used to construct the output dataset name
+config.Data.outputDatasetTag = 'CRAB3_Analysis_test18_mc_ppRef_ZMM_SingleT'
+
+# These values only make sense for processing data
+#    Select input data based on a lumi mask
+#config.Data.lumiMask = 'Cert_Collisions2023HI_374288_375823_Golden.json'
+
+# Where the output files will be transmitted to
+config.Site.storageSite = 'T3_IT_Trieste'
