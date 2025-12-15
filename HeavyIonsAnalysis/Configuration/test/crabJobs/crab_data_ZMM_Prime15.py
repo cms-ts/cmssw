ccf7ccb597785cbec95b6b412d6a90cf5c485bfe
kdeleo <kseniadeleo@gmail.com>
2025-12-15 14:44:54 +0100

    Add CRAB config files

---
+++ HeavyIonsAnalysis/Configuration/test/crabJobs/crab_data_ZMM_Prime15.py
@@ -0,0 +1,26 @@
+from CRABClient.UserUtilities import config
+config = config()
+
+config.General.requestName = 'test13_ZMM_Prime15'
+
+config.JobType.pluginName = 'Analysis'
+# Name of the CMSSW configuration file
+config.JobType.psetName = 'forest_miniAOD_run3_DATA_ZMM.py'
+config.JobType.maxMemoryMB = 2500
+config.JobType.maxJobRuntimeMin = 1200
+config.JobType.allowUndistributedCMSSW = True
+
+config.Data.inputDataset = '/HIPhysicsRawPrime15/HIRun2023A-PromptReco-v2/MINIAOD'
+config.Data.splitting = 'LumiBased'
+config.Data.unitsPerJob = 10
+config.Data.publication = False
+config.Data.inputDBS = 'global'
+# This string is used to construct the output dataset name
+config.Data.outputDatasetTag = 'CRAB3_Analysis_test13_ZMM_Prime15'
+
+# These values only make sense for processing data
+#    Select input data based on a lumi mask
+config.Data.lumiMask = 'Cert_Collisions2023HI_374288_375823_Golden.json'
+
+# Where the output files will be transmitted to
+config.Site.storageSite = 'T3_IT_Trieste'
